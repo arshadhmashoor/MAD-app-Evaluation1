c041d3815e13376922c49dc27ea2c89a3a23e48a
mearshmash <mearsh.mashoor21@gmail.com>
2020-09-04 16:11:39 +0530

    interfaces of e-commerce app for Evaluation 1

---
+++ app/src/main/java/com/example/maapp2/AdminCategoryActivity.java
@@ -0,0 +1,20 @@
+package com.example.maapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ImageView;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+
+    private ImageView tShirts, sportsTShirts, femaleDresses, sweathers;
+    private ImageView glasses, hatsCaps, walletsBagsPurses, shoes;
+    private ImageView headPhonesHandFree, Laptops, watches, mobilePhones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+    }
+}
